7158d0b38afe2164e6b8f1d0370a447a787dd889
eeee0717 <chentao020717Work@outlook.com>
2024-07-05 09:51:04 +0800

    chore: add pgsql

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable node/prefer-global/process */
+import { defineConfig } from 'drizzle-kit'
+
+export default defineConfig({
+  dialect: 'postgresql',
+  schema: './server/database/schema.ts',
+  out: './server/database/migrations',
+  dbCredentials: {
+    url: process.env.POSTGRES_URL!,
+  },
+})
