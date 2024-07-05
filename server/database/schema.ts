b82af94d6633a8cbf8e4340c43ec78926359f606
eeee0717 <chentao020717Work@outlook.com>
2024-07-05 10:30:06 +0800

    chore: init db

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,25 +1,41 @@
 import { drizzle } from 'drizzle-orm/vercel-postgres'
 import { sql } from '@vercel/postgres'
 import {
+  index,
   pgTable,
   serial,
   text,
-  timestamp,
-  uniqueIndex,
 } from 'drizzle-orm/pg-core'
 
-export const UsersTable = pgTable(
-  'users',
+export const podcastsTable = pgTable(
+  'podcasts',
   {
-    id: serial('id').primaryKey(),
-    name: text('name').notNull(),
-    email: text('email').notNull(),
+    pid: serial('pid').primaryKey(),
+    title: text('title').notNull(),
+    author: text('author').notNull(),
+    description: text('description').notNull(),
     image: text('image').notNull(),
-    createdAt: timestamp('createdAt').defaultNow().notNull(),
   },
-  (users) => {
-    return {
-      uniqueIdx: uniqueIndex('unique_idx').on(users.email),
-    }
+  // (table) => {
+  //   return {
+  //     pidIndex: index('pid_idx').on(table.pid),
+  //   }
+  // },
+)
+
+export const episodesTable = pgTable(
+  'episodes',
+  {
+    eid: serial('eid').primaryKey(),
+    pid: text('pid').references(() => podcastsTable.pid),
+    title: text('title').notNull(),
+    description: text('description').notNull(),
+    enclosure: text('enclosure').notNull(),
+    image: text('image').notNull(),
   },
+  // (table) => {
+  //   return {
+  //     eidIndex: index('eid_idx').on(table.eid),
+  //   }
+  // },
 )
