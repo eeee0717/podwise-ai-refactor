5b1ee67b7a6a6c8e04eddb7a65a12682c0928819
eeee0717 <chentao020717Work@outlook.com>
2024-07-06 10:26:42 +0800

    chore

--- pages/podcasts/index.vue
+++ pages/podcasts/index.vue
@@ -11,13 +11,12 @@ onMounted(async () => {
 
 <template>
   <div class="flex justify-center p-10 h-screen">
-    <div class="justify-center" grid="~ 2xl:cols-3 xl:cols-2 lg:cols-2 md:cols-1 gap-15">
+    <div class="h-auto justify-center" grid="~ 2xl:cols-3 xl:cols-2 lg:cols-2 md:cols-1 gap-15 auto-rows-20">
       <PodcastCard
         v-for="podcast in podcastStore.podcasts"
         :key="podcast.pid"
         :podcast="podcast"
-        :height="100" :width="100"
-        class="h-110px  "
+        class="h-110px"
       />
     </div>
   </div>
