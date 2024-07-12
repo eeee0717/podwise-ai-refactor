79458ca6184b89c8cc4a7838a99ac181cb2f58c7
eeee0717 <chentao020717Work@outlook.com>
2024-07-12 10:40:50 +0800

    chore

--- pages/podcasts/[pid].vue
+++ pages/podcasts/[pid].vue
@@ -6,35 +6,41 @@ import type { Episode, Podcast } from '~/types'
 
 const router = useRouter()
 const podcastStore = usePodcastStore()
+const { podcasts } = storeToRefs(podcastStore)
 const podcast = ref<Podcast>()
 const episodes = ref<Episode[]>()
-const episodesTest = ref<Episode[]>()
 onMounted(async () => {
   const pid = router.currentRoute.value.params.pid as string
-  await fetchDbPodcasts(pid)
-  podcast.value = podcastStore.podcasts.find(podcast => podcast.pid === pid)
-  const { episodes: data } = await handleFetchEpisodes(podcast.value!.pid)
+  // 检查 podcasts 中是否已存在对应数据
+  if (!podcasts.value.find(podcast => podcast.pid === pid)) {
+    await fetchDbPodcasts(pid)
+  }
+  podcast.value = podcasts.value.find(podcast => podcast.pid === pid)
+
+  // 并行获取 episodes 数据
+  const [{ episodes: data }] = await Promise.all([handleFetchEpisodes(podcast.value!.pid)])
   if (data) {
     episodes.value = data
-    episodesTest.value = episodes.value?.slice(0, 5)
   }
 })
 </script>
 
 <template>
-  <div class="max-h-100vh flex-row justify-center h-full">
-    <div
-      v-if="podcast"
-      class="w-full"
-    >
-      <IntroductionCard :entity="podcast" />
+  <KeepAlive>
+    <div class="max-h-100vh flex-row justify-center h-full">
+      <div
+        v-if="podcast"
+        class="w-full"
+      >
+        <IntroductionCard :entity="podcast" />
+      </div>
+      <div class="justify-center m-2" grid="~ 2xl:cols-3 xl:cols-2 lg:cols-2 md:cols-1 gap-15 auto-rows-20">
+        <EpisodeCard
+          v-for="episode in episodes"
+          :key="episode.eid"
+          :episode="episode"
+        />
+      </div>
     </div>
-    <div class="justify-center m-2" grid="~ 2xl:cols-3 xl:cols-2 lg:cols-2 md:cols-1 gap-15 auto-rows-20">
-      <EpisodeCard
-        v-for="episode in episodes"
-        :key="episode.eid"
-        :episode="episode"
-      />
-    </div>
-  </div>
+  </KeepAlive>
 </template>
