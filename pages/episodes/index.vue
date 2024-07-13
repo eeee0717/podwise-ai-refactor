90fcd41785c2df20c1e51e30fc8501606ebe6f88
eeee0717 <chentao020717Work@outlook.com>
2024-07-13 13:40:32 +0800

    feat: show isLiked episodes

--- pages/episodes/index.vue
+++ pages/episodes/index.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
 import { queryLikedEpisodes } from '~/composable/useEpisode'
-import type { Episode } from '~/types'
+import { type Episode, SearchState } from '~/types'
 
 const episodes = ref<Episode[]>([])
+const episodesData = ref<Episode[]>([])
+
+const searchValue = ref<string>('')
+const searchState = ref<SearchState>(SearchState.Idle)
+async function onSearch() {
+  episodesData.value = episodes.value?.filter(e => e.title?.toLocaleLowerCase().includes(searchValue.value.toLocaleLowerCase())) ?? []
+}
 onMounted(async () => {
   const { episodes: data } = await queryLikedEpisodes()
   if (data) {
     episodes.value = data
+    episodesData.value = data
     console.warn(data)
   }
 })
@@ -14,6 +22,15 @@ onMounted(async () => {
 
 <template>
   <div>
-    <h1>Episodes</h1>
+    <div class="w-full p-2 my-2 flex flex-row   gap-2 justify-center items-center">
+      <SearchInput v-model="searchValue" :on-search="onSearch" :search-state="searchState" />
+    </div>
+    <div class="justify-center m-2" grid="~ 2xl:cols-3 xl:cols-2 lg:cols-2 md:cols-1 gap-15 auto-rows-20">
+      <EpisodeCard
+        v-for="episode in episodesData"
+        :key="episode.eid"
+        :episode="episode"
+      />
+    </div>
   </div>
 </template>
--- server/api/episode/queryLiked.post.ts
+++ server/api/episode/queryLiked.post.ts
@@ -7,6 +7,7 @@ export default defineCachedEventHandler(async () => {
   const db = drizzle(sql, { schema })
   const episodes = await db.select().from(schema.episodesTable).where(eq(schema.episodesTable.isLiked, true))
   return { episodes }
-}, {
-  maxAge: 1 * 1,
 })
+// , {
+//   maxAge: 1 * 1,
+// })
