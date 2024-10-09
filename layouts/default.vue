90c255ce710372cb357837f93eee499b972850ac
eeee0717 <chentao020717Work@outlook.com>
2024-10-09 18:36:20 +0800

    fix bug: fix tooltipprovider move problem

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,36 +13,38 @@ function onExpand() {
 </script>
 
 <template>
-  <main class="text-center">
-    <TheNav />
-    <div class="hidden flex-col md:flex">
-      <TooltipProvider :delay-duration="0">
-        <ResizablePanelGroup
-          id="resize-panel-group-1"
-          direction="horizontal"
-          class="h-full items-stretch"
-        >
-          <ResizablePanel
-            id="resize-panel-1"
-            :default-size="15"
-            :collapsed-size="4"
-            collapsible
-            :min-size="15"
-            :max-size="15"
-            :class="cn(isCollapsed && 'min-w-[15px] transition-all duration-200 ease-out')"
-            @expand="onExpand"
-            @collapse="onCollapse"
+  <ClientOnly>
+    <main class="text-center">
+      <TheNav />
+      <div class="hidden flex-col md:flex">
+        <TooltipProvider :delay-duration="0">
+          <ResizablePanelGroup
+            id="resize-panel-group-1"
+            direction="horizontal"
+            class="h-full items-stretch"
           >
-            <VerticalNav :is-collapsed="isCollapsed" :links="verticalLinks" />
-          </ResizablePanel>
-          <ResizableHandle id="resize-handle-1" class="h-screen" with-handle />
-          <ResizablePanel id="resize-panel-2" :min-size="30">
-            <div class="h-90% overflow-y-auto">
-              <slot />
-            </div>
-          </ResizablePanel>
-        </ResizablePanelGroup>
-      </TooltipProvider>
-    </div>
-  </main>
+            <ResizablePanel
+              id="resize-panel-1"
+              :default-size="15"
+              :collapsed-size="4"
+              collapsible
+              :min-size="15"
+              :max-size="15"
+              :class="cn(isCollapsed && 'min-w-[15px] transition-all duration-200 ease-out')"
+              @expand="onExpand"
+              @collapse="onCollapse"
+            >
+              <VerticalNav :is-collapsed="isCollapsed" :links="verticalLinks" />
+            </ResizablePanel>
+            <ResizableHandle id="resize-handle-1" class="h-screen" with-handle />
+            <ResizablePanel id="resize-panel-2" :min-size="30">
+              <div class="h-90% overflow-y-auto">
+                <slot />
+              </div>
+            </ResizablePanel>
+          </ResizablePanelGroup>
+        </TooltipProvider>
+      </div>
+    </main>
+  </ClientOnly>
 </template>
