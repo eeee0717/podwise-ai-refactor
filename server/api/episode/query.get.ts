864d6b529cdbc948d1bc4a4c4ab995ca77eb2897
eeee0717 <chentao020717Work@outlook.com>
2024-10-10 10:34:03 +0800

    chore: add performance test

--- server/api/episode/query.get.ts
+++ server/api/episode/query.get.ts
@@ -6,6 +6,11 @@ import * as schema from '../../database/schema'
 export default defineEventHandler(async (event) => {
   const { eid } = getQuery(event)
   const db = drizzle(sql, { schema })
+  const startTime = Date.now()
   const episode = await db.select().from(schema.episodesTable).where(eq(schema.episodesTable.eid, eid as string))
+  const endTime = Date.now()
+  const queryTime = endTime - startTime
+  console.warn(`Database query time: ${queryTime}ms`)
+
   return { episode: episode[0] }
 })
